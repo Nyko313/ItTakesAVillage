c5385b1af0656992c46eb4f748b5027cdb53db52
Ferdinand Koeppen <ferdinand.koeppen@sensolligent.com>
2023-12-17 14:03:06 +0100

    chore: removed build, main menu workin. No scnee transition, credits, or sinstructions

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject startPanel;
+    public TextMeshProUGUI numberOfPlayersText;
+    public Image checkMarkTwinMode; 
+        
+    [Header("Configuration, UI starts with that")]
+    public int maxNumberOfPlayers = 5;
+    public int minNumberOfPlayers = 2;
+    [Header("User Settable")]
+    public int numberOfPlayers = 3;
+    public bool isTwinMode = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkMarkTwinMode.gameObject.SetActive(isTwinMode);
+    }
+    
+    public void ShowStartPanel()
+    {
+        startPanel.SetActive(true);
+    }
+    
+    public void HideStartPanel()
+    {
+        startPanel.SetActive(false);
+    }
+
+    public void IncreaseNumberOfPlayers()
+    {
+        numberOfPlayers = Math.Min(maxNumberOfPlayers, numberOfPlayers + 1);
+        numberOfPlayersText.text = "" + numberOfPlayers;
+    }
+    
+    public void DecreaseNumberOfPlayers()
+    {
+        numberOfPlayers = Math.Max(minNumberOfPlayers, numberOfPlayers - 1);
+        numberOfPlayersText.text = "" + numberOfPlayers;
+    }
+
+    public void ToggleTwinMode()
+    {
+        isTwinMode = !isTwinMode;
+    }
+}
